d79b05a776e12ed58cd44bcdb3745d284d88a1da
Neil Malur <neilmalur@hotmail.com>
2019-05-23 13:16:24 -0400

    remove unnecessary functions

--- main.js
+++ main.js
@@ -155,27 +155,3 @@ function headerBig() {
 	headsize = false;
 	setTimeout(function() {document.getElementById("menuicon").style.transition = "0.5s";}, 5);
 }
-function fade(element) {
-	var op = 1;  // initial opacity
-	var timer = setInterval(function () {
-		if (op <= 0.1){
-			clearInterval(timer);
-			element.style.display = 'none';
-		}
-		element.style.opacity = op;
-		element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-		op -= op * 0.1;
-	}, 50);
-}
-function unfade(element) {
-	var op = 0.1;  // initial opacity
-	element.style.display = 'block';
-	var timer = setInterval(function () {
-		if (op >= 1){
-			clearInterval(timer);
-		}
-		element.style.opacity = op;
-		element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-		op += op * 0.1;
-	}, 10);
-}
