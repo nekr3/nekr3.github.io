8f8a4fd0085d6a1e12d6d46644674d797b5f1d52
Neil Malur <neilmalur@hotmail.com>
2019-05-27 17:17:55 -0400

    update header

---
+++ homebody2.js
@@ -0,0 +1,13 @@
+function resumeIn() {
+	var res = document.getElementById("resume");
+	res.style.border = "1.5px solid #051940";
+	res.style.background = "#ffffff";
+	res.style.color = "#051940";
+}
+function resumeOut() {
+	var res = document.getElementById("resume");
+	res.style.border = "1px solid #ffffff";
+	res.style.background = "#051940";
+	//res.style.color = "#E9F2EA";
+	res.style.color = "#ffffff";
+}
