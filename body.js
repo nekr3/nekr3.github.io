59223943d2f8d9074972b344a03621e226749352
Neil Malur <neilmalur@hotmail.com>
2019-06-06 17:34:52 -0400

    add js body

---
+++ body.js
@@ -0,0 +1,28 @@
+/*window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("pageheader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}*/
+window.onscroll = function() {scrollFunction()};
+
+var header = document.getElementById("pageheader");
+var sticky = header.offsetTop;
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 && !usingx) {
+    if (!headsize) headerSmall();
+	header.classList.add("sticky");
+	headsize = true;
+  } else {
+    if (headsize) headerBig();
+	header.classList.remove("sticky");
+	headsize = false;
+  }
+}
